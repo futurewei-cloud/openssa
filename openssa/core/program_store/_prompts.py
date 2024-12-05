d0a5c48e19cc9871960b9bac3eec7446d49c76e3
spanningwings <ning.z.wu@gmail.com>
2024-12-05 19:46:47 +0000

    Added shortcut for simple questions to exit.

--- openssa/core/program_store/_prompts.py
+++ openssa/core/program_store/_prompts.py
@@ -1,13 +1,31 @@
 PROGRAM_SEARCH_PROMPT_TEMPLATE: str = (
-"""Consider that you can access informational resources summarized in the below dictionary,
-in which each key is a resource's unique name and the corresponding value is that resource's overview:
+"""
+You are an experienced financial analyst. Your job is to check whether any program in the list will help you answer the question.
 
+Here is a description of the resource:
 ```
 {resource_overviews}
 ```
+You are a selection model tasked with identifying the correct program to answer a question. You can choose one program from the list below or NONE if no program directly answers the question. If you are not confident that the program helps, simply reply NONE. You will be rewarded when no program is found, and you will be punished if you select
+a wrong program. In other words, you would rather return NONE than a randomly picked program name. The default answer should be NONE.
+
+**Rules for Selection:**
+1. Select a program only if it directly and completely answers the question. Partial matches, related computations, or inputs to other formulas do not qualify.
+2. If the question cannot be answered directly using any program, return "NONE."
+3. Do not choose a program based on related or indirect connections.
+
+**Example:**
+Question: What is the total asset value of a company?
+Programs:
+1. total-debt: Computes total debt using total asset value and liabilities.
+2. net-income-margin: Calculates the net income margin.
+
+Output: NONE
+
 
 and consider that you are trying to solve the following question/problem/task:
 
+**Question:**
 ```
 {problem}
 ```
@@ -15,12 +33,13 @@ and consider that you are trying to solve the following question/problem/task:
 and that you have access to a collection of problem-solving programs
 summarized by the below name-description pairs:
 
+**Programs:**
 ```
 {program_descriptions}
 ```
 
 Please return the name of the most appropriate program for solving the posed question/problem/task,
-ONLY IF at least one program is deemed applicable/suitable.
+ONLY IF at least one program is deemed applicable/suitable. 
 
 Otherwise, if no applicable/suitable programs are found in the collection,
 please return the word NONE.
